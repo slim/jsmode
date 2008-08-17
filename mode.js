4f0bfb4892a4d3c874a504bf0694a2d901cf9ae5
Slim Amamou <slim.amamou@gmail.com>
2008-08-17 18:11:11 +0200

    first working

--- mode.js
+++ mode.js
@@ -1,19 +1,12 @@
 function Mode(setup) {
-	if (!Mode.prototype.modes) Mode.prototype.modes = [];
-	this.index = Mode.prototype.modes.length;
-	Mode.prototype.modes.push(this);
-
 	this.original_enter = setup.enter;
 	this.enter = function() {
-		this.exitAll();
-		return this.original_enter();
+		if (Mode.current) {
+			Mode.current.exit();
+		}
+		Mode.current = this;
+		this.original_enter();
 	}
 
 	this.exit = setup.exit;
-	Mode.prototype.exitAll = function() {
-		var modes = Mode.prototype.modes; 
-		for (i in modes) {
-			modes[i].exit();
-		}
-	}
 }
